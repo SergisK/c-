93c39a1da33fc96389672dd1190d0507d5e98cd2
SergisK <sergiusz.kanski@gmail.com>
2018-04-11 18:11:04 +0200

    Pierwsza zmiana

---
+++ zadanie pierwsze/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout<<"Prosze podaj liczbe calkowita!";
+    int calkowita;
+    float wymierna;
+        cin>>calkowita;
+        cin>>wymierna;
+        cout<<"calkowita to: "<<calkowita<<" wymierna to: "<<wymierna;
+    return 0;
+}
